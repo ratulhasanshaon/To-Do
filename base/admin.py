6a0832f7ef25091332ab6ff7bda1bb99103646ca
Ratul Hasan <ratulhasanshaon@gmail.com>
2022-07-09 11:58:44 +0600

    settings edited commit

--- base/admin.py
+++ base/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Task
+from django.contrib.auth.models import Group
 
+
+admin.site.site_header = 'To-Do-List Admin Dashboard'
 admin.site.register(Task)
+admin.site.unregister(Group)
