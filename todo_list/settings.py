5ffb3c7ffd2280544e1d6b8692bb0176677c7a8c
Ratul Hasan <ratulhasanshaon@gmail.com>
2022-07-11 10:18:18 +0600

    settings edited commit

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = Flase
 
 ALLOWED_HOSTS = ['ratul-todo-app.herokuapp.com', '127.0.0.1:8000']
 
